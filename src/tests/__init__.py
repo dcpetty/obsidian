43eaf8cc4db8a6ef7cd4da5ec497a1906c75a49f
DCPetty <github@patton-petty.net>
2024-05-30 09:29:25 -0400

    Updated __version__ and added test_runner to __all__.

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,9 +1,16 @@
-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# __init__.py
+#
+"""
+__init__.py imports modules and sets dunder variables.
+"""
 from obsidianjekyll import *
 from . import *
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 __all__ = ["paths", "pathnames", "files", "log",
-           'test_pathnames', 'test_paths', 'test_files',
+           'test_pathnames', 'test_paths', 'test_files', 'test_runner',
            '__version__']
